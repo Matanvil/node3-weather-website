e40683c2254830b7aa1da7a394b888d2dffd2044
matanv <matanv@gmail.com>
2021-12-13 10:36:39 +0200

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,17 +8,15 @@ weatherForm.addEventListener("submit", (event) => {
   const location = search.value;
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.errorMessage) {
-          messageOne.textContent = "Error: " + data.errorMessage;
-          messageTwo.textContent = ""
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.errorMessage) {
+        messageOne.textContent = "Error: " + data.errorMessage;
+        messageTwo.textContent = "";
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
